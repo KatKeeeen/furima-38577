3fdd35fe2103ff4e1cb6dc4cd150a42cadf33efd
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-01-29 21:32:56 +0900

    バリデーションの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,8 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :price_before_type_cast, format: { with: /\A[0-9]+\z/, message: " is invalid. Input half-width characters" }, 
-                                       numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
+    validates :price, numericality: { only_integer: true, message: " is invalid. Input half-width characters" }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
     validates :category_id,        numericality: { other_than: 1, message: "can't be blank" }
     validates :state_id,           numericality: { other_than: 1, message: "can't be blank" }
     validates :shopping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
