dd931b8ef11442101e529a6c0906a2fdeaaab22d
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-01-30 22:35:38 +0900

    手数料計算メソッドの変更

--- app/javascript/exhibition.js
+++ app/javascript/exhibition.js
@@ -1,8 +1,8 @@
 function calculation (){
   const itemPrice  = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
-    const salesCommission = Math.round(itemPrice.value * 0.1);
-    const salesProfit = itemPrice.value - salesCommission;
+    const salesCommission = Math.floor(itemPrice.value * 0.1);
+    const salesProfit = Math.floor(itemPrice.value - salesCommission);
     const commissionNum  = document.getElementById("add-tax-price");
     const profitNum  = document.getElementById("profit");
     commissionNum.innerHTML = `${salesCommission}`;
