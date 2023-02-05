7a65ed3945d2a8046a0669438ede128e9bffde22
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-02-05 20:39:19 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,17 +49,17 @@ class ItemsController < ApplicationController
 
   def move_to_sign_in
     return if user_signed_in?
+
     redirect_to new_user_session_path
   end
 
   def move_to_index
-    unless current_user.id == @item.user.id
-      redirect_to action: :index
-    end
+    return if current_user.id == @item.user.id
+
+    redirect_to action: :index
   end
 
   def item_setting
     @item = Item.find(params[:id])
   end
-  
 end
