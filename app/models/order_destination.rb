d626936aea86ce6ce6e95d56d8e29a08cd0618ce
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-02-12 22:06:05 +0900

    電話番号のバリデーション修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -10,10 +10,9 @@ class OrderDestination
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number,length: { in: 10..11, message:'is too short' }
+    validates :phone_number, length: { in: 10..11, message:'is too short' },
+                             numericality: { only_integer: true, message: 'is invalid. Input only number' }
   end
-
-  validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
                              
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
