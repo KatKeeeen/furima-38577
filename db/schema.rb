6bbb53f48033854e7d3e20503eee9c372e8a05ef
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-02-13 21:19:14 +0900

    ビル名のnull制約解除

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_02_07_132059) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
